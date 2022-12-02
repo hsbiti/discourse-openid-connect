5479373e7424fc2e6161c3736099835f21711217
Hamza Sbiti <hamzasbiti@gmail.com>
2022-12-02 14:06:23 +0100

    Delete fix_timeout.rb

--- lib/fix_timeout.rb
+++
@@ -1,9 +0,0 @@
-class FinalDestination::Resolver
-    module OpenIdResolverLookup
-        def self.lookup(addr, timeout: nil)
-            Rails.logger.debug("==== Fix timeout ====")
-            super(addr,20);
-        end
-    end
-    prepend OpenIdResolverLookup
-end
